da80e1cc4ddfe88150bab0268224834c9f245758
neihaoo <neihaoo@gmail.com>
2022-07-18 21:44:44 +0300

    fix linter error

--- users/forms.py
+++ users/forms.py
@@ -4,12 +4,14 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
+MAX_LENGTH = 150
+
 
 class UserCreateForm(UserCreationForm):
     """User form."""
 
-    first_name = forms.CharField(max_length=150, required=True)
-    last_name = forms.CharField(max_length=150, required=True)
+    first_name = forms.CharField(max_length=MAX_LENGTH, required=True)
+    last_name = forms.CharField(max_length=MAX_LENGTH, required=True)
 
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
