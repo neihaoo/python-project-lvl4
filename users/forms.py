216ace7dcf0a87d2b0ae25039fd88894d3e0604b
neihaoo <neihaoo@gmail.com>
2022-07-18 21:31:44 +0300

    add fullname for users

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,15 @@
 """User application forms."""
 
+from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
 
 class UserCreateForm(UserCreationForm):
     """User form."""
+
+    first_name = forms.CharField(max_length=150, required=True)
+    last_name = forms.CharField(max_length=150, required=True)
 
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
--- users/models.py
+++ users/models.py
@@ -5,3 +5,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     """Model representing a user account."""
+
+    def __str__(self):
+        """Represent the model as a string."""
+        return self.get_full_name()
