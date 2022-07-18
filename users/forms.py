598edfa4721f4a59f02bb083c9c068d8cdb4505b
neihaoo <neihaoo@gmail.com>
2022-07-18 21:56:47 +0300

    fix i18n

--- users/forms.py
+++ users/forms.py
@@ -3,6 +3,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.translation import gettext_lazy as _
 
 MAX_LENGTH = 150
 
@@ -10,8 +11,16 @@ MAX_LENGTH = 150
 class UserCreateForm(UserCreationForm):
     """User form."""
 
-    first_name = forms.CharField(max_length=MAX_LENGTH, required=True)
-    last_name = forms.CharField(max_length=MAX_LENGTH, required=True)
+    first_name = forms.CharField(
+        max_length=MAX_LENGTH,
+        required=True,
+        label=_('Name'),
+    )
+    last_name = forms.CharField(
+        max_length=MAX_LENGTH,
+        required=True,
+        label=_('Surname'),
+    )
 
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
